7f144e5917ebfd13577da24138af7f6b9623de81
Graham Whelan <gawhelan@gmail.com>
2017-12-10 13:57:39 +0000

    [impl] Refactor error handling
    - Instead of directly setting the response status code and body,
    validation errors now cause an exception to be thrown with details of
    the error
    - Removed dependency on swagger2-koa

--- README.md
+++ README.md
@@ -24,13 +24,23 @@ const Koa = require('koa');
 const body = require('koa-body');
 const openapi = require('koa-openapi');
 
+const specFilepath = __dirname + '/spec.yaml';
+
 const app = new Koa();
 
 app.use(body());
-app.use(openapi({ filepath: __dirname + '/spec.yaml' }));
+app.use(openapi({ filepath: specFilepath, verbose: true }));
 
 ...
 ```
+
+If a validation error is encountered then an exception is thrown. If a
+request fails to validate, then a status `400` error is thrown. If a
+response fails to validate, then a status `500` error is thrown.
+
+Details of what caused a validation error can be obtained by setting the
+`verbose` flag to `true`. When set, request/response validation errors
+will have an additional `details` property indicating why validation failed.
 
 ## License
 MIT
--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-const swagger = require('swagger2');
-const swaggerKoa = require('swagger2-koa');
+const {
+  loadDocumentSync,
+  validateDocument,
+  compileDocument,
+  validateRequest,
+  validateResponse,
+} = require('swagger2');
 
-module.exports = function (options) {
-  const opts = options || {};
-
-  const document = swagger.loadDocumentSync(opts.filepath);
-  if (!swagger.validateDocument(document)) {
-    throw new Error('Specification is not a valid OpenAPI v2 document!');
+module.exports = function validate({ filepath, verbose = false }) {
+  const document = loadDocumentSync(filepath);
+  if (!validateDocument(document)) {
+    throw new Error(`Invalid OpenAPI 2.0 document: ${filepath}`);
   }
 
-  return swaggerKoa.validate(document);
-}
+  const compiled = compileDocument(document);
+
+  return async (ctx, next) => {
+    const compiledPath = compiled(ctx.path);
+    if (compiledPath === undefined) {
+      ctx.throw(404);
+    }
+
+    const { method, headers, query, body: reqBody } = ctx.request;
+    const reqErrors = validateRequest(
+      compiledPath,
+      method,
+      query,
+      reqBody,
+      headers,
+      ctx.params,
+    );
+
+    if (reqErrors === undefined) {
+      ctx.throw(405);
+    }
+
+    if (reqErrors.length > 0) {
+      if (verbose) {
+        ctx.throw(400, { details: { request: reqErrors } });
+      } else {
+        ctx.throw(400);
+      }
+    }
+
+    await next();
+
+    const { status, body: resBody } = ctx;
+    const resErrors = validateResponse(compiledPath, method, status, resBody);
+    if (resErrors) {
+      if (verbose) {
+        ctx.throw(500, { details: { response: resErrors } });
+      } else {
+        ctx.throw(500);
+      }
+    }
+  };
+};
